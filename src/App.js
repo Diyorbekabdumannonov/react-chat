30fe3a6c1d49cd1ebafb066e566497ec83111499
Diyorbekabdumannonov <94838816+Diyorbekabdumannonov@users.noreply.github.com>
2021-12-26 14:26:32 +0500

    nice

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import SubCategory from './components/subCatecory/SubCategory';
 
 function App() {
   return (
-    < div className='all'>
-      <div className='sidebar'>
-        <Categories />
-        <SubCategory />
-      </div>
-      <Node className='rightbar'/>
+    <div className='all'>
+      <nav class="navbar-expand-lg navbar-dark">
+        {/* <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button> */}
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <div className='sidebar'>
+            <Categories />
+            <SubCategory />
+          </div>
+        </div>
+      </nav>
+      <Node className='rightbar' />
     </div>
   );
 }
