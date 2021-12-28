76e0a01aa00333a42b34c0ab07cf14a036f5e9ad
Diyorbekabdumannonov <94838816+Diyorbekabdumannonov@users.noreply.github.com>
2021-12-28 17:48:40 +0500

    nice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Categories from './components/categories/Categories';
 import Node from './components/nodes/Node';
 import SubCategory from './components/subCatecory/SubCategory';
 
 function App() {
   return (
-    <div className='all'>
-      <nav class="navbar-expand-lg navbar-dark">
-        {/* <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button> */}
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <div className='sidebar'>
-            <Categories />
-            <SubCategory />
-          </div>
+    <>
+      <div className='all'>
+        <div className='sidebar'>
+          <Categories />
+          <SubCategory />
         </div>
-      </nav>
-      <Node className='rightbar' />
-    </div>
+        <Node className='rightbar' />
+      </div>
+
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="/menu" component={<Categories />} />
+        </Routes>
+      </BrowserRouter> */}
+
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Categories from './components/categories/Categories';
+
 
 ReactDOM.render(
-  <App />,
+    <App/>,
   document.getElementById('root')
 );
